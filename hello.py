b7a82036781c915888e6369966052604a2f5e122
Akira Kato <akira.kato.19930831@icloud.com>
2015-03-28 06:07:06 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from bottle import route, run, template, request
+# データ保持
+user_list = []
+
+# 192.168.33.10:8080
+@route('/hello/', method='GET')
+@route('/hello/', method='POST')
+def hello():
+    user_dict = {}
+    user_dict['username'] = request.forms.get('username')
+    user_dict['title'] = request.forms.get('title')
+    user_dict['body'] = request.forms.get('body')
+
+    user_list.append(user_dict)
+
+    return template('hello', user_list=user_list)
+
+run(host='192.168.33.10', port=8081, debug=True, reloader=True)
