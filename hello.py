d3bc2c077e79fd33b6588acd0d652a5350847464
Akira Kato <akira.kato.19930831@icloud.com>
2015-03-28 08:41:53 +0000

    add map

--- hello.py
+++ hello.py
@@ -12,9 +12,10 @@ def hello():
     user_dict['username'] = request.forms.get('username')
     user_dict['title'] = request.forms.get('title')
     user_dict['body'] = request.forms.get('body')
+    user_dict['address'] = request.forms.get('address')
 
     user_list.append(user_dict)
 
     return template('hello', user_list=user_list)
 
-run(host='192.168.33.10', port=8081, debug=True, reloader=True)
+run(host='192.168.33.10', port=8080, debug=True, reloader=True)
